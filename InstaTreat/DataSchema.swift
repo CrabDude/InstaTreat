ec84a3f03f716bea3699236c2811490a9dade483
Ashar Rizqi <asharrizqi@gmail.com>
2015-03-08 22:09:30 -0700

    main item view

--- InstaTreat/DataSchema.swift
+++ InstaTreat/DataSchema.swift
@@ -8,19 +8,77 @@
 
 import UIKit
 
+var itemCategories = [
+    "cookies",
+    "brownies",
+    "breads",
+    "pastries",
+    "muffins",
+    "candies",
+    "chocolate",
+    "brittles",
+    "cakes",
+    "pies",
+    "rice krispies",
+    "marshmallows"
+]
+
 class ItemPost {
+    let id: AnyObject!
     let quantity: Int
     let createdAt: NSDate
     let endDate: NSDate
     let item: Item
     let price: String
+    var longitude: Float!
+    var latitude: Float!
     
     var options = [String:Bool]()
+    
+    func initWithDictionary(dict: [String: AnyObject]) {
+        
+    }
 }
-
 
 
 class Item {
-    let images: [UIImage]
-    let description: String
+    let id: AnyObject!
+    let images: [UIImage]!
+    let description: String!
+    var title: String!
+    var rating: Int!
+    var owner: String!
+    var tags: Array<String>!
+    var categories = itemCategories
+    
 }
+
+class User {
+//    let email: String!
+    let phoneNumber: String!
+    let firstName: String!
+    let lastName: String!
+    let email: String!
+    let address: String!
+    let ccId: String!
+    var isBaker: Bool!
+    var userId: AnyObject!
+}
+
+class Baker: User {
+    let availability: AnyObject!
+}
+
+class Buyer:User {
+    var canPost: Bool!
+}
+
+class Sale {
+    var quantity: Int!
+    var saleId: AnyObject!
+    var state:String!
+    var purchasedAt: NSDate!
+    var buyerId: AnyObject!
+    var itemPostId: AnyObject!
+}
+
