1b894e74f86dc10bde34fbb9baebcb5167790707
Ashar Rizqi <asharrizqi@gmail.com>
2015-03-10 17:24:10 -0700

    login view controller

---
+++ InstaTreat/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  InstaTreat
+//
+//  Created by Ashar Rizqi on 3/10/15.
+//  Copyright (c) 2015 Adam Crabtree. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        
+    }
+
+    @IBAction func onLoginPressed(sender: UIButton) {
+    }
+    
+}
