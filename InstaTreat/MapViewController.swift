3e7b7d6e4cb58f01453c37be849de8430c7e0e19
Ashar Rizqi <asharrizqi@gmail.com>
2015-03-10 22:14:44 -0700

    hooked up mapview controller, streamview controller and item detail

---
+++ InstaTreat/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  InstaTreat
+//
+//  Created by Ashar Rizqi on 3/10/15.
+//  Copyright (c) 2015 Adam Crabtree. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    @IBAction func onListButtonPressed(sender: UIButton) {
+        self.navigationController?.dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    
+
+}
--- InstaTreat/StreamViewController.swift
+++ InstaTreat/StreamViewController.swift
@@ -10,18 +10,41 @@ import UIkit
 
 class StreamViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    var items: Array<AnyObject>!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
-        
+        var query = PFQuery(className: "Items")
+        self.items = query.findObjects()
+        self.tableView.reloadData()
     }
+    
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCellWithIdentifier("itemCell") as ItemCell
         return cell
     }
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let vc = AppHelper.storyboard.instantiateViewControllerWithIdentifier("ItemDetailViewController") as UIViewController
+        self.navigationController?.presentViewController(vc, animated: true, completion: nil)
     }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    @IBAction func onLogoutPressed(sender: UIButton) {
+        PFUser.logOut()
+        let vc = AppHelper.storyboard.instantiateViewControllerWithIdentifier("LoginViewController") as UIViewController
+        self.navigationController?.pushViewController(vc, animated: true)
+        
+    }
 
+    @IBAction func onMapButtonPressed(sender: UIButton) {
+        let vc = AppHelper.storyboard.instantiateViewControllerWithIdentifier("MapViewController") as UIViewController
+        self.navigationController?.pushViewController(vc, animated: true)
+        
+    }
 }
