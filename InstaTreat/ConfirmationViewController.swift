c2e2d477474fef4473880d54c3a413d673c960f1
Ashar Rizqi <asharrizqi@gmail.com>
2015-03-12 23:12:23 -0700

    finished payment view

--- InstaTreat/ConfirmationViewController.swift
+++ InstaTreat/ConfirmationViewController.swift
@@ -8,10 +8,32 @@
 
 import UIKit
 
-class ConfirmationViewController: UIViewController {
+class ConfirmationViewController: UIViewController, UIAlertViewDelegate {
+    
+    
+    var alert = UIAlertController(title: "Thank You!", message: "Your order is on its way", preferredStyle: UIAlertControllerStyle.Alert)
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
     }
 
+    @IBAction func onBuy(sender: UIButton) {
+        
+        let okAction = UIAlertAction(title: "OK", style: .Default) { (action) in
+            self.dismiss()
+        }
+        alert.addAction(okAction)
+//        self.presentViewController(alert, animated: true) { () -> Void in
+//            self.dismiss()
+//        }
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+    }
+    
+    func dismiss(){
+        var vc = AppHelper.storyboard.instantiateViewControllerWithIdentifier("StreamNavigationController") as UINavigationController
+        self.navigationController?.presentViewController(vc, animated: true, completion: nil)
+    }
 }
