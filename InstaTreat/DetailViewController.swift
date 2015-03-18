ef44143743de65fe59dc6c86bcdb9a34055308ab
Ashar Rizqi <asharrizqi@gmail.com>
2015-03-17 23:52:42 -0700

    modified views to get rid of extra offset

--- InstaTreat/DetailViewController.swift
+++ InstaTreat/DetailViewController.swift
@@ -24,7 +24,7 @@ class DetailViewController: UIViewController, UITableViewDelegate, UITableViewDa
     
     override func viewDidLoad() {
         println("loading view")
-        self.tableView.rowHeight = 350.0
+        self.tableView.rowHeight = 250.0
         
 //        self.tableView.frame.height = CGFloat(350.0)
         println(item.otherImages)
